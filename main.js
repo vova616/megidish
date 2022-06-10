ed52b62191330bdd1ce83b342950957c6c82315a
Vovchik <vova616@gmail.com>
2022-06-11 02:54:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,17 +11,21 @@ client.on('guildMemberAdd', (member) => {
     member.setNickname(member.user.username+ ' Megidish');
 });
 client.on('message' , (message) => {
-    if(message.author.id == '145185932281249792') {  // alt account id
+    try {
+        if(message.author.id == '145185932281249792') {  // alt account id
         message.channel.send( message.author.toString()+ " Eize dafuk hhhhhhh loh gadol");
-    }
-    if(message.author.id == '552178160205168671') {  // alt account id
-        message.channel.send( message.author.toString()+ " Ani a kogmaw ahi tov");
-    }
-    if (Math.random() < 0.3 && client.user != message.author) {
-        message.channel.send("Lo nahon, " + message.author.toString() + " a kogmaw ahi tov");
-    }
-    if (!message.author.username.includes(" Megidish")) {
-        message.member.setNickname(message.author.username+ ' Megidish');
+        }
+        if(message.author.id == '552178160205168671') {  // alt account id
+            message.channel.send( message.author.toString()+ " Ani a kogmaw ahi tov");
+        }
+        if (Math.random() < 0.3 && client.user != message.author) {
+            message.channel.send("Lo nahon, " + message.author.toString() + " a kogmaw ahi tov");
+        }
+        if (!message.author.username.includes(" Megidish")) {
+            message.member.setNickname(message.author.username+ ' Megidish');
+        }
+    } catch(e) {
+        console.error(e)   
     }
 });
 
