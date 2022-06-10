30b8c3be2083009891dc3c1e6abbf83e9d594722
Vovchik <vova616@gmail.com>
2022-06-11 02:57:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,19 +10,19 @@ client.on('guildMemberAdd', (member) => {
     member.guild.channels.cache.get('186170823806156801').send( member.user.toString()+ " has been Megidish'ed."); // bot test server id
     member.setNickname(member.user.username+ ' Megidish');
 });
-client.on('message' , (message) => {
+client.on('message' , async (message) => {
     try {
         if(message.author.id == '145185932281249792') {  // alt account id
-        message.channel.send( message.author.toString()+ " Eize dafuk hhhhhhh loh gadol");
+            await message.channel.send( message.author.toString()+ " Eize dafuk hhhhhhh loh gadol");
         }
         if(message.author.id == '552178160205168671') {  // alt account id
-            message.channel.send( message.author.toString()+ " Ani a kogmaw ahi tov");
+            await message.channel.send( message.author.toString()+ " Ani a kogmaw ahi tov");
         }
         if (Math.random() < 0.3 && client.user != message.author) {
-            message.channel.send("Lo nahon, " + message.author.toString() + " a kogmaw ahi tov");
+            await message.channel.send("Lo nahon, " + message.author.toString() + " a kogmaw ahi tov");
         }
         if (!message.author.username.includes(" Megidish")) {
-            message.member.setNickname(message.author.username+ ' Megidish');
+            await message.member.setNickname(message.author.username+ ' Megidish');
         }
     } catch(e) {
         console.error(e)   
